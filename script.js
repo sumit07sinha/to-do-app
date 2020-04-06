b241f8fc73de6c001ce0d5d81e4a63b95e2f8852
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-04-06 08:57:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,78 @@
-// create a close button and append it on each li tag
 window.onload=()=>{
-     // to add element
-     document.getElementById("addToDoItem").addEventListener('click',createTodoElement)
-    
-    let myToDoList= document.getElementsByTagName('li');
-    Array.from(myToDoList).forEach((element) => {
-        let span = document.createElement("SPAN");
-        let txt = document.createTextNode("x");
-        // let checkBox= document.createElement("input");
-        // checkBox.setAttribute('type','checkbox');
-        // checkBox.className="checked";
-        // ulListItems.appendChild(checkBox);
-        span.className = "delete";
-        span.appendChild(txt);
-        element.appendChild(span);
-    });
-    deleteItem();
-    // to add checkbox in all the items
-        document.querySelector('ul').addEventListener('click',checkTask);
-}
-  // to create to-do element
-  let createTodoElement= ()=>{
-    let newItem= document.createElement('li');
-    newItem.innerText= document.getElementById("inputByUser").value;
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("x");
-    span.className = "delete";
-    
-    if (newItem.innerText === '') {
-        alert("You must write something!");
-        } else {
-           let liItem= document.getElementById("listedItems")
-           liItem.appendChild(newItem);
-            span.appendChild(txt);
-            newItem.appendChild(span);
-        }
-  }
-  // to delete todo item
-  let deleteItem =()=>{
-            let deleteTodoItem= document.getElementsByClassName("delete");
-            Array.from(deleteTodoItem).forEach((e)=>{
-            e.addEventListener('click', (event)=>{
-                let ulListItems= document.getElementById("listedItems");
-                ulListItems.removeChild(event.target.parentElement);
-        })}) 
-  }
-// to check completed task
-  let checkTask= (ev)=> {
-    if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+  
+    let todoElementbyValue= (value)=>{
+      todoListToBeSaved.push(value)
+      let newItem= document.createElement('li');
+      newItem.innerText= value;
+      let span = document.createElement("SPAN");
+      let txt = document.createTextNode("x");
+      span.className = "delete";
+      
+      if (newItem.innerText === '') {
+          alert("You must write something!");
+          } else {
+             let liItem= document.getElementById("listedItems")
+             liItem.appendChild(newItem);
+              
+              newItem.appendChild(span);
+              span.appendChild(txt);
+          }
     }
-    };
-    let myToDoList= document.getElementsByTagName('li').value;
-window.localStorage.setItem('todoitem', 'myToDoList[]');
+  
+  
+       // to add element
+       const todoListToBeSaved = [];
+  
+       const mylistitems = localStorage.getItem("mylistitems");
+  
+       let arrayfromLocalStorage = mylistitems && mylistitems.split(",");
+       if(arrayfromLocalStorage && arrayfromLocalStorage.length>0){
+        arrayfromLocalStorage.forEach((arrayElement)=>{
+          todoElementbyValue(arrayElement);
+        })
+       }
+  
+       const createTodoElement = ()=>{
+        const inputValue = document.getElementById("inputByUser").value;
+        todoElementbyValue(inputValue);
+        localStorage.setItem("mylistitems",todoListToBeSaved);
+       }
+  
+      
+  
+      
+  
+  
+  
+  
+  
+  
+      let myToDoList= document.getElementsByTagName('li');
+      Array.from(myToDoList).forEach((element) => {
+          let span = document.createElement("SPAN");
+          let txt = document.createTextNode("x");
+          span.className = "delete";
+          span.appendChild(txt);
+          element.appendChild(span);
+      });
+      
+  
+    // to create to-do element
+    
+  
+      
+    const ulClicked = (event)=>{
+      const lisOfClassForClickedElement = Array.from(event.target.classList);
+      
+      if(lisOfClassForClickedElement.includes("delete")){
+        event.currentTarget.removeChild(event.target.parentElement);
+        arrayfromLocalStorage.pop(event.target.parentElement.innerText);
+        
+        localStorage.setItem("mylistitems",arrayfromLocalStorage);
+      }else{
+          event.target.classList.toggle('checked')
+      }
+    }
+    document.getElementById("listedItems").addEventListener("click",ulClicked);
+    document.getElementById("addToDoItem").addEventListener('click',createTodoElement);
+  } 
